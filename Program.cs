e7f98c4d5deb45b06e55d5c4f62aea3851c6f81c
Rachana R <rachanarrachu28@gmail.com>
2020-10-26 20:49:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UserRegistrationValidation
+{
+    class Program
+    {
+        /// <summary>Defines the entry point of the application.</summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            //Printing Welcome we
+            Console.WriteLine("Welcome To User Registration Validation");
+        }
+    }
+}
