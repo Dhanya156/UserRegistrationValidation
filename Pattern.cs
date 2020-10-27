18e2ccc4677f025298f74afd4b4cec612b3eb8d7
Dhanya S V <dhanyavirupaksha@gmail.com>
2020-10-27 14:04:13 +0530

    Added : Modified Pattern To Validate Phone Number

--- Pattern.cs
+++ Pattern.cs
@@ -8,7 +8,7 @@ namespace UserRegistrationValidation
     class Pattern
     {
         ///Country code follow by space and 10 digit number
-        public static string REGEX_PHONENUMBER = "^\\\\+(?:[0-9] ?){6,14}[0-9]$";
+        public static string REGEX_PHONENUMBER = "^[1-9]?[0-9]{9}";
 
         /// Method to find match with user input and regular expression specified
         public bool ValidatePhoneNumber(string phoneNumber)
