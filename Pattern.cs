6d0b965c9310c91be25877040c8977888337485a
Dhanya S V <dhanyavirupaksha@gmail.com>
2020-10-27 13:25:28 +0530

    Added : Validate Password

--- Pattern.cs
+++ Pattern.cs
@@ -8,12 +8,12 @@ namespace UserRegistrationValidation
     class Pattern
     {
         /// Starts with cap And have minimun of 3 character
-        public static string REGEX_LASTNAME = "^[A-Z]+[a-z]{2,}";
+        public static string REGEX_PASSWORD = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[@#%^&*!()<>?:;]).{8,}$";
        
         /// Method to find match with user input and regular expression specified
-        public bool ValidateLastName(string lastName)
+        public bool ValidatePassword(string password)
         {
-            return Regex.IsMatch(lastName, REGEX_LASTNAME);
+            return Regex.IsMatch(password, REGEX_PASSWORD);
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -10,11 +10,11 @@ namespace UserRegistrationValidation
         {
             ///Creating object for class pattern
             Pattern pattern = new Pattern();
-            Console.WriteLine("Enter the Last name to validate");
+            Console.WriteLine("Enter the Password to validate");
             ///Reading input from user
-            string lastName = Console.ReadLine();
-            //Calling a method to validate
-            Console.WriteLine(pattern.ValidateLastName(lastName));
+            string password = Console.ReadLine();
+            ///Calling a method to validate
+            Console.WriteLine(pattern.ValidatePassword(password));
         }
     }
 }
